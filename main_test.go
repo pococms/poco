a2c3a3d3d20026af758051284adc0d39f45fa04d
pococms <pococms@gmail.com>
2022-08-10 17:57:27 -0700

    Test for missing title

--- main_test.go
+++ main_test.go
@@ -12,14 +12,17 @@ Title: PocoCMS title
 ---
 `
 
+var fmTitleMissing = `---
+---
+`
+
 type stringTest struct {
-	key string
+	key      string
 	expected string
 }
 
 var stringTests = []stringTest{
 	{"Title", "PocoCMS title"},
-	{"Title", "Bad PocoCMS title"},
 }
 
 func fmTest(key string, fm map[string]interface{}) string {
@@ -28,6 +31,11 @@ func fmTest(key string, fm map[string]interface{}) string {
 	fmt.Printf("fm[%s] = %s\n", key, s)
 	return s
 }
+
+// TestAllFrontMatterStrings tests all string values
+// one can expect in front matter. Since it's only
+// strings that means keys like Sheets aren't
+// tested here.
 func TestAllFrontMatterStrings(t *testing.T) {
 	source := fmAllStr
 	var err error
@@ -55,4 +63,21 @@ func TestHelloName(t *testing.T) {
 		fmt.Println(s)
 	}
 
+}
+
+func TestMissingTitleTag(t *testing.T) {
+	tagLine := poweredBy
+	source := fmTitleMissing
+	var fm map[string]interface{}
+	var err error
+	if _, fm, err = mdYAMLToHTML([]byte(source)); err != nil {
+		t.Errorf("Failed converting %s to HTML and obtaining front matter", source)
+	}
+
+	//value := fmTest("Title", fm)
+  expected := "\t<title>" + tagLine + "</title>\n"
+  actual := titleTag(fm)
+	if actual != expected {
+		t.Errorf("FrontMatter[\"Title\"]: expected \"%s\", actual \"%s\"", expected, actual)
+	}
 }
